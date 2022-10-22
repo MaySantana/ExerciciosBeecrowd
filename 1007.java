ef6a50ad46e6e2192e7ece165cc9e38628f12d86
Mayra de Santana Claudino <mayrasantana.dev@gmail.com>
2022-10-22 20:25:09 -0300

    Create 1007.java

---
+++ 1007.java
@@ -0,0 +1,22 @@
+//Resolver a seguinte expressão entre quatro números inseridos pelo usuário: (A * B - C * D), Java
+//May Santana, 2021
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]){
+      
+        int a, b, c, d, DIFERENCA;
+        Scanner scan = new Scanner(System.in);
+        
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextInt();
+        d = scan.nextInt();
+      
+        DIFERENCA = (a * b - c * d);
+      
+        System.out.println("DIFERENCA = " + DIFERENCA);
+        
+    }
+}
