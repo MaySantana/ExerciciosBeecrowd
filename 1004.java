13e552fb5b40e0307211ead993a9c553d32fbc69
Mayra de Santana Claudino <mayrasantana.dev@gmail.com>
2022-10-22 20:18:38 -0300

    Create 1004.java

---
+++ 1004.java
@@ -0,0 +1,20 @@
+//Produto de dois inteiros, Java
+//May Santana, 2021
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]){
+      
+        int a, b, PROD;
+        Scanner scan = new Scanner(System.in);
+        
+        a = scan.nextInt();
+        b = scan.nextInt();
+      
+        PROD = a * b;
+      
+        System.out.println("PROD = " + PROD);
+        
+    }
+}
