8f13d493867def1d3727cb665947d301183d91a1
Mayra de Santana Claudino <mayrasantana.dev@gmail.com>
2022-10-22 20:47:03 -0300

    Create 1016.java

---
+++ 1016.java
@@ -0,0 +1,21 @@
+//Distancia entre dois carros, Java
+//May Santana, 2022
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int velocidadeX, tempoY;
+		
+		velocidadeX = scan.nextInt();
+		
+		tempoY = velocidadeX * 2;
+		
+		System.out.println(tempoY + " minutos");
+		
+	}
+}
