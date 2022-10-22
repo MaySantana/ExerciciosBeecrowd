e7cbaf7a51b47e1487a5a49859696dbad4a56c3a
Mayra de Santana Claudino <mayrasantana.dev@gmail.com>
2022-10-22 20:15:13 -0300

    Create 1002.java

---
+++ 1002.java
@@ -0,0 +1,33 @@
+//Área de um Círculo, Java
+//May Santana, 2021
+
+import java.util.*;
+import java.text.DecimalFormat;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+        
+        double R;
+        
+        Scanner scan = new Scanner(System.in);
+        
+        R = scan.nextDouble();
+        
+        double A = AreaCirculo(R);
+        
+        System.out.printf("A=%.4f\n", A);
+        
+    }
+    
+    public static double AreaCirculo (double R) {
+        
+        double π = 3.14159;
+        double A;
+        
+        A = π * R * R;
+        
+        return A;
+        
+    }  
+}
