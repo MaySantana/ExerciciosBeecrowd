11e080c325489421aa24fab53e636f0ab1aaf00b
Mayra de Santana Claudino <mayrasantana.dev@gmail.com>
2022-10-22 20:13:30 -0300

    Create 1001.java

---
+++ 1001.java
@@ -0,0 +1,21 @@
+//Soma de dois inteiros, Java
+//May Santana, 2021
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]){
+      
+        int a, b, x;
+      
+        Scanner scan = new Scanner(System.in);
+        
+        a = scan.nextInt();
+        b = scan.nextInt();
+      
+        x = a + b;
+      
+        System.out.println("X = " + x);
+        
+    }
+}
