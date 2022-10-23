521d95eb25d5515af0240ca8ad4135ffc894eb9a
Mayra de Santana Claudino <mayrasantana.dev@gmail.com>
2022-10-22 21:20:27 -0300

    Create 1050.java

---
+++ 1050.java
@@ -0,0 +1,46 @@
+//Prefixos telefônicos, Java
+//May Santana, 2022
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int ddd;
+		
+		ddd = scan.nextInt();
+		
+		switch (ddd){
+		    case 61:
+		        System.out.println("Brasilia");
+		        break;
+		    case 71:
+		        System.out.println("Salvador");
+		        break;
+		    case 11:
+		        System.out.println("Sao Paulo");
+		        break;
+		    case 21:
+		        System.out.println("Rio de Janeiro");
+		        break;
+		    case 32:
+		        System.out.println("Juiz de Fora");
+		        break;
+		    case 19:
+		        System.out.println("Campinas");
+		        break;
+		    case 27:
+		        System.out.println("Vitoria");
+		        break;
+		    case 31:
+		        System.out.println("Belo Horizonte");
+		        break;
+		    default:
+		        System.out.println("DDD nao cadastrado");
+		        break;
+		}
+	}
+}
