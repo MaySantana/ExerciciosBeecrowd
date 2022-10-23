a991777e9e30470931a0df90ce59236a2383f3cf
Mayra de Santana Claudino <mayrasantana.dev@gmail.com>
2022-10-22 21:11:01 -0300

    Create 1045.java

---
+++ 1045.java
@@ -0,0 +1,37 @@
+//Tipos de Triângulo, Java
+//May Santana, 2022
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+	    
+		Scanner scan = new Scanner(System.in);
+		
+		double A = scan.nextDouble();
+		double B = scan.nextDouble();
+		double C = scan.nextDouble();
+		double aux;
+		
+		if (B > A && B > C) {
+			aux = A;
+			A = B;
+			B = aux;
+		} else if (C > A) {
+			aux = A;
+			A = C;
+			C = aux;
+		}
+		
+		if (A >= (B + C)) { 
+			System.out.println("NAO FORMA TRIANGULO");
+		} else {
+			if (A*A == (B*B) + (C*C)) System.out.println("TRIANGULO RETANGULO");
+			if (A*A > (B*B) + (C*C)) System.out.println("TRIANGULO OBTUSANGULO");
+			if (A*A < (B*B) + (C*C)) System.out.println("TRIANGULO ACUTANGULO");
+			if (A == B && A == C) System.out.println("TRIANGULO EQUILATERO");
+			if ((A == B && A != C) || (A == C && A != B) || (B == C && B != A)) System.out.println("TRIANGULO ISOSCELES");
+		}
+	}
+}
