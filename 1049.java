7e8949a48bddb546d54bb7ee3c2b9ec0d2d9628b
Mayra de Santana Claudino <mayrasantana.dev@gmail.com>
2022-10-22 21:16:34 -0300

    Create 1049.java

---
+++ 1049.java
@@ -0,0 +1,46 @@
+//Classificação de Animais, Java
+//May Santana, 2022
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+	    
+		Scanner leitor = new Scanner(System.in);
+		
+		String palavra1 = leitor.next();
+		String palavra2 = leitor.next();
+		String palavra3 = leitor.next();
+		
+		if (palavra1.equalsIgnoreCase("vertebrado")) {
+			if (palavra2.equalsIgnoreCase("ave")) {
+				if (palavra3.equalsIgnoreCase("carnivoro")) {
+					System.out.println("aguia");
+				} else {
+					System.out.println("pomba");
+				}
+			} else {
+				if (palavra3.equalsIgnoreCase("onivoro")) {
+					System.out.println("homem");
+				} else {
+					System.out.println("vaca");
+				}
+			}
+		} else {
+			if (palavra2.equalsIgnoreCase("inseto")) {
+				if (palavra3.equalsIgnoreCase("hematofago")) {
+					System.out.println("pulga");
+				} else {
+					System.out.println("lagarta");
+				}
+			} else {
+				if (palavra3.equalsIgnoreCase("hematofago")) {
+					System.out.println("sanguessuga");
+				} else {
+					System.out.println("minhoca");
+				}			
+			}
+		}
+	}
+}
