e84f7a515a3133d14bb4f74754ac2b017db4ff73
Mayra de Santana Claudino <mayrasantana.dev@gmail.com>
2022-10-22 22:13:01 -0300

    Create 1096.java

---
+++ 1096.java
@@ -0,0 +1,20 @@
+//Sequência 2, Java
+//May Santana, 2022
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+		int i, j;
+		i = 1;
+    
+		while (i <= 9){
+		    for (j = 7; j > 4; j--){
+		        System.out.println("I=" + i + " J=" + j);
+		    }
+		    
+		    i+=2;
+		    
+		}
+	}
+}
