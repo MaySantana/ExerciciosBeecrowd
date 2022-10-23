685635ed7931887d9b5f7369aeb59fb8834b1239
Mayra de Santana Claudino <mayrasantana.dev@gmail.com>
2022-10-22 21:25:56 -0300

    Create 1059.java

---
+++ 1059.java
@@ -0,0 +1,17 @@
+//Números Pares, Java
+//May Santana, 2022
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		int i;
+		for (i = 2; i <= 100; i = i + 2){
+		    
+		    System.out.printf("%d\n", i);
+      
+		}
+	}
+}
