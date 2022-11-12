d9dd728bc69990b9472b16349ddca97baf1ed04c
Mayra de Santana Claudino <mayrasantana.dev@gmail.com>
2022-11-12 18:33:19 -0300

    Create 2738.sql

---
+++ 2738.sql
@@ -0,0 +1,8 @@
+--Contest, SQL
+--May Santana, 2022
+SELECT c.name,
+ROUND((s.math*2 + s.specific*3 + s.project_plan*5) / 10, 2) avg
+FROM candidate c
+INNER JOIN score s
+ON c.id = s.candidate_id
+ORDER BY avg DESC;
