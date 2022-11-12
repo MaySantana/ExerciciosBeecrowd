ebb4d4f12e3ad42809ec2d1b74e624c34ff6baac
Mayra de Santana Claudino <mayrasantana.dev@gmail.com>
2022-11-12 18:20:37 -0300

    Create 2616.sql

---
+++ 2616.sql
@@ -0,0 +1,7 @@
+--No Rental, SQL
+--May Santana, 2022
+SELECT cus.id, cus.name
+FROM customers cus
+FULL OUTER JOIN locations loc ON loc.id_customers = cus.id
+WHERE loc.locations_date IS NULL
+ORDER BY cus.id;
