eb6a45512e90f85cb4e3f2ebd0b003120175697a
Mayra de Santana Claudino <mayrasantana.dev@gmail.com>
2022-11-12 18:28:54 -0300

    Create 2623.sql

---
+++ 2623.sql
@@ -0,0 +1,7 @@
+--Categories with Various Products, SQL
+--May Santana, 2022
+SELECT pro.name, cat.name
+FROM products pro
+INNER JOIN categories cat ON pro.id_categories = cat.id
+WHERE pro.amount > 100 AND cat.id IN (1, 2, 3, 6, 9)
+ORDER BY cat.id;
